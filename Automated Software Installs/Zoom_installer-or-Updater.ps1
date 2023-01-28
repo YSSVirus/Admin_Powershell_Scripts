c35786a0dcd8fd22367cf01ed007a9d16f18dc55
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2023-01-28 15:49:00 -0800

    Updated zoom script
    I updated this, it now first checks the zoom version and installation status before doing anything then it will check to see if it's installed if so it removes it, then we go ahead with the installation. This all happens silently in the background and is fully automated

--- Automated Software Installs/Zoom_installer-or-Updater.ps1
+++ Automated Software Installs/Zoom_installer-or-Updater.ps1
@@ -1,69 +1,82 @@
 function main(){ #This is our main function for organization
-	$ErrorActionPreference = "Stop"	 
+	$ErrorActionPreference = "Stop"
 	$Version_Powershell_Major = $PSVERSIONTABLE.PSVersion.Major
 	function Error_Handling($text){
 	$PSItem.ScriptStackTrace
 	$PSItem.Exception.Message
-	Write-Host "$text" -ForegroundColor Red
-}
+	$ProgressPreference = 'SilentlyContinue'
+	}
 	function Removing_Old_Zoom(){ #This will uninstall any old Zoom version avoiding side by side issues
-		$Old_Zoom = "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Zoom\"
-		cd $Old_Zoom
-		try{
-			$Zoom_Uninstall_Command = "$home\AppData\Roaming\Zoom\uninstall\Installer.exe /uninstall"
-			Start-Process -NoNewWindow Powershell.exe "-Command $Zoom_Uninstall_Command" -Wait
+		$zip_path = "$home/Downloads/CleanZoom.zip"
+		$exe_path = "$home/Downloads/CleanZoom.exe"
+		if ((test-path $zip_path) -eq $true){
+			rm $zip_path
+		}#remove incase old zoom cleaner is out of date
+		if ((test-path $exe_path) -eq $true){
+			rm $exe_path
+		}
+		$Zoom_Uninstaller = "https://support.zoom.us/hc/en-us/article_attachments/360084068792/CleanZoom.zip"
+		$Zoom_Uninstaller_Path = "$home\Downloads\CleanZoom.zip"
+		$command = "invoke-webrequest" + " -uri " + "$Zoom_Uninstaller" + " -OutFile " + "$zip_path"
+		Try { 
+			Invoke-Expression -Command $command | Out-Null
+			while (!(Test-Path $Zoom_Uninstaller_Path)) { Start-Sleep 1 }
+		}#Downloading the uninstaller
+		catch{
+			$Error_Text = 'Could not download the un-installer for zoom'
+			Error_Handling($Error_Text)
+			exit
+		}#error if it cant download un-installer
+		Expand-Archive -Path "$home/Downloads/CleanZoom.zip" -DestinationPath "$home/Downloads/"
+		Start-Process ".\CleanZoom.exe" -ArgumentList "/silent /keep_outlook_plugin /keep_lync_plugin /keep_notes_plugin" -wait
+	}
+	function Downloading_Zoom(){
+		$Download_URL = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
+		cd "$home/Downloads"
+		$command = "invoke-webrequest" + " -uri " + "$Download_URL" + " -OutFile " + "$home/Downloads/ZoomInstaller.msi"
+		Try { #Now we try running that with first chrome, then the default browser, then it will print the link out if nothing else works
+			Invoke-Expression "$command"
+			while (!(Test-Path "$home/Downloads/ZoomInstaller.msi")) { Start-Sleep 1 }
 		}
 		catch{
-			$Error_Text = 'Could not locate Zoom uninstaller'
+			$Error_Text = 'Could not download the installer for zoom'
 			Error_Handling($Error_Text)
 			exit
 		}
-	}
-	function Downloading_Zoom(){
-		$Download_URL = "https://ninite.com/zoom/ninite.exe"
-		$path = "$home/Downloads/"
-		$path = $path + "Ninite Zoom Installer.exe"
-		cd "$home/Downloads"
-		Try { #Now we try running that with first chrome, then the default browser, then it will print the link out if nothing else works
-			Start-Process -NoNewWindow -Wait invoke-webrequest -uri "$Download_URL" -Wait
-			while (!(Test-Path $path)) { Start-Sleep 1 }
-		}
-		catch{
-			try{
-				#Start-Process -NoNewWindow -Wait "$Download_URL"
-				while (!(Test-Path $path)) { Start-Sleep 1 }
-			}
-			catch{
-				$Error_Text = 'Could not download the installer for zoom'
-				Error_Handling($Error_Text)
-				exit
-			}
-		}
 		try{
-			Start-Process -Wait '.\Ninite Zoom Installer.exe'
+			Start-Process "./ZoomInstaller.msi" -ArgumentList "/qn /passive /quiet" -Wait
 		}
 		catch{
 			echo 'Zoom could not install'
 		}
 	}
-	$checking = Test-Path "$home\\AppData\\Roaming\\Zoom\\uninstall\\Installer.exe"
-	Try{
-		$File_Checker = Get-Item "$home\\Downloads\\ZoomInstaller.exe"
+	function House_Cleaning(){
+		rm "$home\Downloads\CleanZoom.zip"
+		rm "$home\Downloads\cleanzoom.log"
+		rm "$home\Downloads\ZoomInstaller.msi"
+	}#cleans up old files
+
+	$checking = Test-Path "$home/Downloads/ZoomInstaller.msi" ###change
+	$Start_Dir = $PWD
+	$Download_Dir = "$home/Downloads/"
+	cd $Download_Dir
+	Try{ 
+		$File_Checker = "$home/Downloads/ZoomInstaller.msi"
 		rm $File_Checker
-	}
-	Catch{
+	}#testing to see if there is any old previously downloaded installers then removes them
+	Catch{ 
 		$File_Checker = 'NULL'
-	}
-	try{
-		if ($checking){
-			Removing_Old_Zoom 
-		}
-	}
-	catch{
-		Write-Host "No Zoom installed installing now"
-	}
-	cd "$home\Downloads\"
-	Downloading_Zoom
+	}#This is mainly a placeholder in-case there is no old installer
+	if ($checking){
+		Removing_Old_Zoom
+	}# this uninstalls the old version of zoom IF THE USER HAS IT
+	Downloading_Zoom #Here we install zoom
+	House_Cleaning
+	cd $Start_Dir
+
 }
-main #main 
+
+if (!($req -match "$version") -or ((Get-Package -Name "Zoom*" -EA Ignore) -eq $null)){
+	main #main
+}
 exit #exit upon completion
