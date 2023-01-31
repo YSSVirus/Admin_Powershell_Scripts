96375691e9c995f8fdf45ccb0fb498e78a579852
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2023-01-30 22:18:02 -0800

    Updated the script as there were some issues (#2)
    This script now makes then removes a directory in C:\ to work with, this is due to some RMM tools running as a system, that don't have $home or some common issues relating to calling this directory. I have also fixed the version detection.

--- Automated Software Installs/Zoom_installer-or-Updater.ps1
+++ Automated Software Installs/Zoom_installer-or-Updater.ps1
@@ -7,16 +7,10 @@ function main(){ #This is our main function for organization
 	$ProgressPreference = 'SilentlyContinue'
 	}
 	function Removing_Old_Zoom(){ #This will uninstall any old Zoom version avoiding side by side issues
-		$zip_path = "$home/Downloads/CleanZoom.zip"
-		$exe_path = "$home/Downloads/CleanZoom.exe"
-		if ((test-path $zip_path) -eq $true){
-			rm $zip_path
-		}#remove incase old zoom cleaner is out of date
-		if ((test-path $exe_path) -eq $true){
-			rm $exe_path
-		}
+		$zip_path = "C:\temp\CleanZoom.zip"
+		$exe_path = "C:\temp\CleanZoom.exe"
 		$Zoom_Uninstaller = "https://support.zoom.us/hc/en-us/article_attachments/360084068792/CleanZoom.zip"
-		$Zoom_Uninstaller_Path = "$home\Downloads\CleanZoom.zip"
+		$Zoom_Uninstaller_Path = "C:\temp\CleanZoom.zip"
 		$command = "invoke-webrequest" + " -uri " + "$Zoom_Uninstaller" + " -OutFile " + "$zip_path"
 		Try { 
 			Invoke-Expression -Command $command | Out-Null
@@ -27,16 +21,16 @@ function main(){ #This is our main function for organization
 			Error_Handling($Error_Text)
 			exit
 		}#error if it cant download un-installer
-		Expand-Archive -Path "$home/Downloads/CleanZoom.zip" -DestinationPath "$home/Downloads/"
+		Expand-Archive -Path "C:\temp\CleanZoom.zip" -DestinationPath "C:\temp\"
 		Start-Process ".\CleanZoom.exe" -ArgumentList "/silent /keep_outlook_plugin /keep_lync_plugin /keep_notes_plugin" -wait
 	}
 	function Downloading_Zoom(){
 		$Download_URL = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
-		cd "$home/Downloads"
-		$command = "invoke-webrequest" + " -uri " + "$Download_URL" + " -OutFile " + "$home/Downloads/ZoomInstaller.msi"
+		cd "C:\temp"
+		$command = "invoke-webrequest" + " -uri " + "$Download_URL" + " -OutFile " + "C:\temp\ZoomInstaller.msi"
 		Try { #Now we try running that with first chrome, then the default browser, then it will print the link out if nothing else works
 			Invoke-Expression "$command"
-			while (!(Test-Path "$home/Downloads/ZoomInstaller.msi")) { Start-Sleep 1 }
+			while (!(Test-Path "C:\temp\ZoomInstaller.msi")) { Start-Sleep 1 }
 		}
 		catch{
 			$Error_Text = 'Could not download the installer for zoom'
@@ -50,19 +44,12 @@ function main(){ #This is our main function for organization
 			echo 'Zoom could not install'
 		}
 	}
-	function House_Cleaning(){
-		rm "$home\Downloads\CleanZoom.zip"
-		rm "$home\Downloads\cleanzoom.log"
-		rm "$home\Downloads\ZoomInstaller.msi"
-	}#cleans up old files
-
-	$checking = Test-Path "$home/Downloads/ZoomInstaller.msi" ###change
+	$checking = Test-Path "C:\temp\ZoomInstaller.msi" ###change
 	$Start_Dir = $PWD
-	$Download_Dir = "$home/Downloads/"
+	$Download_Dir = "C:\temp\"
 	cd $Download_Dir
 	Try{ 
-		$File_Checker = "$home/Downloads/ZoomInstaller.msi"
-		rm $File_Checker
+		$File_Checker = "C:\temp\ZoomInstaller.msi"
 	}#testing to see if there is any old previously downloaded installers then removes them
 	Catch{ 
 		$File_Checker = 'NULL'
@@ -71,12 +58,21 @@ function main(){ #This is our main function for organization
 		Removing_Old_Zoom
 	}# this uninstalls the old version of zoom IF THE USER HAS IT
 	Downloading_Zoom #Here we install zoom
-	House_Cleaning
 	cd $Start_Dir
 
 }
 
-if (!($req -match "$version") -or ((Get-Package -Name "Zoom*" -EA Ignore) -eq $null)){
+if ((Test-Path "C:\temp\") -eq $true){
+	rm C:\temp -Recurse
+}
+
+$req = Invoke-WebRequest -uri "https://www.deepfreeze.com/Cloud/pr/softwareupdater/Latest"
+$req_content = $req.RawContent
+$zoom = (Get-Package | Where-Object {$_.Name -like "*Zoom*"})
+$zoom_version = $zoom.version
+mkdir "C:\temp" | Out-Null
+if (($req_content -Notmatch "$zoom_version") -or ($zoom_version -eq $null)){
 	main #main
 }
-exit #exit upon completion
+rm C:\temp -Recurse
+#exit #exit upon completion
