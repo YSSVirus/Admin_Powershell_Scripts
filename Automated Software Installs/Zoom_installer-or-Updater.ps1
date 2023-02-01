669529a59d4752c283e28658c0baa844747b0a8f
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2023-01-31 21:24:35 -0800

    Final update until a dependency gets changed.
    I have added registry checking instead of checking the installed apps, this provides better results, as well as not many places to check and it eats lets resources,

--- Automated Software Installs/Zoom_installer-or-Updater.ps1
+++ Automated Software Installs/Zoom_installer-or-Updater.ps1
@@ -7,12 +7,12 @@ function main(){ #This is our main function for organization
 	$ProgressPreference = 'SilentlyContinue'
 	}
 	function Removing_Old_Zoom(){ #This will uninstall any old Zoom version avoiding side by side issues
-		$zip_path = "C:\temp\CleanZoom.zip"
-		$exe_path = "C:\temp\CleanZoom.exe"
+		$zip_path = "C:\temp_zoom\CleanZoom.zip"
+		$exe_path = "C:\temp_zoom\CleanZoom.exe"
 		$Zoom_Uninstaller = "https://support.zoom.us/hc/en-us/article_attachments/360084068792/CleanZoom.zip"
-		$Zoom_Uninstaller_Path = "C:\temp\CleanZoom.zip"
+		$Zoom_Uninstaller_Path = "C:\temp_zoom\CleanZoom.zip"
 		$command = "invoke-webrequest" + " -uri " + "$Zoom_Uninstaller" + " -OutFile " + "$zip_path"
-		Try { 
+		Try {
 			Invoke-Expression -Command $command | Out-Null
 			while (!(Test-Path $Zoom_Uninstaller_Path)) { Start-Sleep 1 }
 		}#Downloading the uninstaller
@@ -21,16 +21,16 @@ function main(){ #This is our main function for organization
 			Error_Handling($Error_Text)
 			exit
 		}#error if it cant download un-installer
-		Expand-Archive -Path "C:\temp\CleanZoom.zip" -DestinationPath "C:\temp\"
+		Expand-Archive -Path "C:\temp_zoom\CleanZoom.zip" -DestinationPath "C:\temp_zoom\"
 		Start-Process ".\CleanZoom.exe" -ArgumentList "/silent /keep_outlook_plugin /keep_lync_plugin /keep_notes_plugin" -wait
 	}
 	function Downloading_Zoom(){
 		$Download_URL = "https://zoom.us/client/latest/ZoomInstallerFull.msi?archType=x64"
-		cd "C:\temp"
-		$command = "invoke-webrequest" + " -uri " + "$Download_URL" + " -OutFile " + "C:\temp\ZoomInstaller.msi"
+		cd "C:\temp_zoom"
+		$command = "invoke-webrequest" + " -uri " + "$Download_URL" + " -OutFile " + "C:\temp_zoom\ZoomInstaller.msi"
 		Try { #Now we try running that with first chrome, then the default browser, then it will print the link out if nothing else works
 			Invoke-Expression "$command"
-			while (!(Test-Path "C:\temp\ZoomInstaller.msi")) { Start-Sleep 1 }
+			while (!(Test-Path "C:\temp_zoom\ZoomInstaller.msi")) { Start-Sleep 1 }
 		}
 		catch{
 			$Error_Text = 'Could not download the installer for zoom'
@@ -44,14 +44,14 @@ function main(){ #This is our main function for organization
 			echo 'Zoom could not install'
 		}
 	}
-	$checking = Test-Path "C:\temp\ZoomInstaller.msi" ###change
+	$checking = Test-Path "C:\temp_zoom\ZoomInstaller.msi" ###change
 	$Start_Dir = $PWD
-	$Download_Dir = "C:\temp\"
+	$Download_Dir = "C:\temp_zoom\"
 	cd $Download_Dir
-	Try{ 
-		$File_Checker = "C:\temp\ZoomInstaller.msi"
+	Try{
+		$File_Checker = "C:\temp_zoom\ZoomInstaller.msi"
 	}#testing to see if there is any old previously downloaded installers then removes them
-	Catch{ 
+	Catch{
 		$File_Checker = 'NULL'
 	}#This is mainly a placeholder in-case there is no old installer
 	if ($checking){
@@ -59,20 +59,28 @@ function main(){ #This is our main function for organization
 	}# this uninstalls the old version of zoom IF THE USER HAS IT
 	Downloading_Zoom #Here we install zoom
 	cd $Start_Dir
-
 }
 
-if ((Test-Path "C:\temp\") -eq $true){
-	rm C:\temp -Recurse
+if ((Test-Path "C:\temp_zoom\") -eq $true){
+	rm C:\temp_zoom -Recurse
 }
 
-$req = Invoke-WebRequest -uri "https://www.deepfreeze.com/Cloud/pr/softwareupdater/Latest"
+$req = Invoke-WebRequest -uri "https://www.deepfreeze.com/Cloud/pr/softwareupdater/Latest" -UseBasicParsing
 $req_content = $req.RawContent
-$zoom = (Get-Package | Where-Object {$_.Name -like "*Zoom*"})
-$zoom_version = $zoom.version
-mkdir "C:\temp" | Out-Null
-if (($req_content -Notmatch "$zoom_version") -or ($zoom_version -eq $null)){
+$key = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Zoom*"
+if ((Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Zoom*") -eq "$null"){
+	$key = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Zoom*"
+	$installLocation = (Get-ItemProperty -Path $key -Name "DisplayVersion").DisplayVersion
+	$zoom_version = $installLocation.Split(" ")[0]
+}
+else{
+	$installLocation = (Get-ItemProperty -Path $key -Name "DisplayVersion").DisplayVersion
+	$zoom_version = $installLocation.Split(" ")[0]
+}
+echo $zoom_version 
+mkdir "C:\temp_zoom" | Out-Null
+if (($req_content -NotContains "Zoom updated to $zoom_version") -or ($zoom_version -eq $null)){
 	main #main
 }
-rm C:\temp -Recurse
-#exit #exit upon completion
+rm C:\temp_zoom -Recurse
+exit #exit upon completion
